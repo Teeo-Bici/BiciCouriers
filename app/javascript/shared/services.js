9b825d88632996e63b1c736b449cde85689387b2
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-03-26 21:14:41 +0100

    start responsive

--- app/javascript/shared/services.js
+++ app/javascript/shared/services.js
@@ -1,5 +1,6 @@
 const movement = () => {
   window.onscroll =  (e) => {
+    console.log(e);
     const oeufDiv = document.querySelector('.oeuf-1');
     const lineDiv = document.querySelector('.plein');
     let h_pos = pageXOffset;
@@ -8,6 +9,13 @@ const movement = () => {
     const EggLeft = oeufDiv.style.left = 600 + (h_pos / 6) + 'px';
   }
 };
+
+
+// $(document).ready(function(){
+//      $(window).scroll(function(){
+//        $('.right').css('transform', 'translate3d(0,' + $(this).scrollTop()*2 + 'px, 0)');
+//     }).scroll();
+// });
 
 
 export { movement };
