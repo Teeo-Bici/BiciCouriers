ba4586151899f5472e355fdbfb4a622df1a8c091
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-03-27 19:09:07 +0100

    service wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 import "bootstrap";
-//import Swup from 'swup';
 
-import  { movement } from '../shared/services.js';
+import  { movement, scrollFn } from '../shared/services.js';
 import  { cardsFly } from '../shared/avantages.js';
 
-movement();
 cardsFly();
+movement();
+
--- app/javascript/shared/services.js
+++ app/javascript/shared/services.js
@@ -1,21 +1,22 @@
+import HorizontalScroll from '@oberon-amsterdam/horizontal';
+const horizontal = new HorizontalScroll({ container: document.querySelector('.service-board') });
+
+
 const movement = () => {
-  window.onscroll =  (e) => {
+  horizontal.on('scroll', (e) => {
     console.log(e);
+
     const oeufDiv = document.querySelector('.oeuf-1');
     const lineDiv = document.querySelector('.plein');
-    let h_pos = pageXOffset;
-
-    const lineSize = lineDiv.style.width = 200 + (h_pos * 1.6) + 'px';
-    const EggLeft = oeufDiv.style.left = 600 + (h_pos / 6) + 'px';
-  }
+    // const LgContainer = document.querySelector('.test-contain');
+    // let h_pos = pageXOffset;
+    // let v_pos = pageYOffset;
+    // console.log(h_pos);
+    // console.log(v_pos);
+    // LgContainer.style.width = 2000 + (e) + 'px';
+    const lineSize = lineDiv.style.width = 200 + (e * 1.6) + 'px';
+    const EggLeft = oeufDiv.style.left = 600 + (e / 6) + 'px';
+  });
 };
-
-
-// $(document).ready(function(){
-//      $(window).scroll(function(){
-//        $('.right').css('transform', 'translate3d(0,' + $(this).scrollTop()*2 + 'px, 0)');
-//     }).scroll();
-// });
-
 
 export { movement };
