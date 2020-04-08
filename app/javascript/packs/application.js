c297f2f0fb2fe0ea3f192fe1b233bee4d75b83bf
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-04-08 14:21:51 +0200

    add map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import "bootstrap";
 import  { movement } from '../shared/services.js';
 import  { cardsFly } from '../shared/avantages.js';
 import  { userLogo } from '../shared/navbar.js';
+import  { mapInit } from '../shared/mapbox.js';
 
 movement();
 cardsFly();
 userLogo();
+mapInit();
